6014a893457626efb4b3f02b46e413a5f05dd291
jh4737@columbia.edu <jh4737@columbia.edu>
2024-11-26 15:25:18 -0500

    disable security for testing, fixed bug for unique username

---
+++ src/test/java/dev/teamproject/config/NoSecurityConfig.java
@@ -0,0 +1,23 @@
+package dev.teamproject.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Disable security for testing.
+ */
+@TestConfiguration
+public class NoSecurityConfig {
+  /**
+   * Filter to disable security for testing.
+   */
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable()) // Disable CSRF protection
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // Allow all requests
+    return http.build();
+  }
+}
